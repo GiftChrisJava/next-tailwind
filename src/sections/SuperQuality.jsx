c230c7771dad36042b83153908b611f2280caebe
GiftChrisJava <chrisjava77@gmail.com>
2024-01-26 00:51:40 +0200

    creating super quality

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,39 @@
+// import { arrowRight } from "../assets/icons";
+import Button from "../components/Button";
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col
+   gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2
+          className="font-palanquin text-4xl capitalize font-bold
+          lg:max-w-lg"
+        >
+          We Provide You
+          <span className="text-coral-red inline-block mt-3">Super</span>
+          <span className="text-coral-red inline-block mt-3">Quality</span>
+          Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life. Discover stylish Nike arrivals, quality comfort, and
+          innovation for your active life.
+        </p>
+
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
