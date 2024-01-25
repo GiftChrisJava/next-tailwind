2a0fdd4006120ac079b59c8ec92ecb915b58d5b5
GiftChrisJava <chrisjava77@gmail.com>
2024-01-26 01:01:44 +0200

    finished with super quality

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,4 +1,4 @@
-// import { arrowRight } from "../assets/icons";
+import { shoe8 } from "../assets/images";
 import Button from "../components/Button";
 
 const SuperQuality = () => {
@@ -13,9 +13,9 @@ const SuperQuality = () => {
           className="font-palanquin text-4xl capitalize font-bold
           lg:max-w-lg"
         >
-          We Provide You
-          <span className="text-coral-red inline-block mt-3">Super</span>
-          <span className="text-coral-red inline-block mt-3">Quality</span>
+          We Provide You{" "}
+          <span className="text-coral-red inline-block mt-3"> Super </span>
+          <span className="text-coral-red inline-block mt-3">Quality</span>{" "}
           Shoes
         </h2>
         <p className="mt-4 lg:max-w-lg info-text">
@@ -31,6 +31,16 @@ const SuperQuality = () => {
         <div className="mt-11">
           <Button label="View details" />
         </div>
+      </div>
+
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
       </div>
     </section>
   );
