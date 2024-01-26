8bcc87de1da5ccd88f6d64eac89af5f51043b9e5
GiftChrisJava <chrisjava77@gmail.com>
2024-01-26 17:09:56 +0200

    creating customer reviews

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,5 +1,21 @@
 const CustomerReviews = () => {
-  return <div>CustomerReviews</div>;
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers </span> Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied Customers about their
+        excceptional experience with us.
+      </p>
+
+      <p
+        className="mt-24 flex flex-1 justify-evenly 
+      items-center max-lg:flex-col gap-14"
+      ></p>
+    </section>
+  );
 };
 
 export default CustomerReviews;
