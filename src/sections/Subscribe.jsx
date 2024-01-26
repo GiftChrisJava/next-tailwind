f92b0e3700a006b0ff3c23f9ebd9cdbfbfaaebfb
GiftChrisJava <chrisjava77@gmail.com>
2024-01-26 18:01:50 +0200

    added index.js

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,4 +1,5 @@
-import { Button } from "../components/Button";
+import { Button } from "../components";
+
 const Subscribe = () => {
   return (
     <section
@@ -10,10 +11,7 @@ const Subscribe = () => {
         <span className="text-coral-red"> Updates </span> & Newsletter
       </h3>
 
-      <div
-        className="lg:max-w-[40%] w-full flex items-center 
-       max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full"
-      >
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="subscribe@nike.com" className="input" />
         <div className="flex max-sm:justify-end items-center max-sm:w-full">
           <Button label="Sign Up" fullWidth />
