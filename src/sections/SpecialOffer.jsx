40086e32f38064765bd43408f091d424f9c73833
GiftChrisJava <chrisjava77@gmail.com>
2024-01-26 12:57:50 +0200

    creating special offer

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,5 +1,21 @@
+import { offer } from "../assets/images";
 const SpecialOffer = () => {
-  return <div>SpecialOffer</div>;
+  return (
+    <section
+      className="flex justify-wrap items-center max-xl:flex-col-reverse
+   gap-10 max-container"
+    >
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt="image"
+          height={687}
+          width={773}
+          className="object-contain w-full"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SpecialOffer;
